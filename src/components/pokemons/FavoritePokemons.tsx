e467a58603af9674299a1913b96cab5311080e0c
Alexis Castellanos <castellanos.alexis@hotmail.com>
2025-08-30 08:01:13 -0600

    feat 26: show and remove favorite pokemons

--- src/components/pokemons/FavoritePokemons.tsx
+++ src/components/pokemons/FavoritePokemons.tsx
@@ -1,6 +1,8 @@
 import type { FavoritePokemon } from "@interfaces/favorite-pokemon";
 import { createSignal, For } from "solid-js";
 
+import { FavoritePokemonCard } from "./FavoritePokemonCard";
+
 const getLocalStorage = (): FavoritePokemon[] => {
 	const favoritesPokemons = JSON.parse(
 		localStorage.getItem("favoritePokemons") ?? "[]"
@@ -16,11 +18,7 @@ export const FavoritePokemons = () => {
 	return (
 		<div class="grid grid-cols-2 sm:grid-cols-4">
 			<For each={pokemons()}>
-				{(pokemon) => (
-					<div class="m-2 flex flex-col items-center justify-center">
-						<span class="capitalize">{pokemon.name}</span>
-					</div>
-				)}
+				{(pokemon) => <FavoritePokemonCard pokemon={pokemon} />}
 			</For>
 		</div>
 	);
